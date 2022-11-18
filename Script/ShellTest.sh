017a91df88d68c58e4f56db07f1d06ffb6b4aef6
vuongtuha <70053986+vuongtuha@users.noreply.github.com>
2022-11-18 18:40:43 +0700

    Update ShellTest.sh

--- Script/ShellTest.sh
+++ Script/ShellTest.sh
@@ -10,7 +10,7 @@ cd fcitx-bamboo;
 git sparse-checkout set Script;
 sudo eselect repository enable gentoo-zh && sudo emerge --sync;
 sudo mkdir -p /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
-sudo cp $HOME/Downloads/fcitx-bamboo.ebuild /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
+sudo cp $HOME/Downloads/fcitx-bamboo/Script/fcitx-bamboo-1.0.1.ebuild /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
 cd /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
 sudo ebuild fcitx-bamboo-1.0.1.ebuild digest && sudo ebuild fcitx-bamboo-1.0.1.ebuild manifest;
 sudo emerge app-i18n/fcitx-bamboo::gentoo-zh
