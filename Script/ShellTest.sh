43e731faf5543c5768faeddae96a533e0599495f
vuongtuha <70053986+vuongtuha@users.noreply.github.com>
2022-11-18 16:59:54 +0700

    Add files via upload

---
+++ Script/ShellTest.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd $HOME/Downloads/
+git clone \
+                                       --depth 1  \
+                                       --filter=blob:none  \
+                                       --sparse \
+                                       https://github.com/fcitx/fcitx5-bamboo \
+                                 ;
+cd fcitx5-bamboo;
+git sparse-checkout set Script;
+sudo eselect repository enable gentoo-zh && sudo emerge --sync;
+sudo mkdir -p /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
+sudo cp $HOME/Downloads/fcitx-bamboo.ebuild /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
+cd /var/db/repos/gentoo-zh/app-i18n/fcitx-bamboo/;
+sudo ebuild fcitx-bamboo-1.0.1.ebuild digest && sudo ebuild fcitx-bamboo-1.0.1.ebuild manifest;
+sudo emerge app-i18n/fcitx-bamboo::gentoo-zh
+exit 1
+
